f1029611683ee2b687a50c41f46ad78f083dde3e
Martin H. Normark <mhn@milkshakesoftware.com>
2014-03-01 12:44:57 +0100

    Setup data parser project

---
+++ data/parser/lib/windmill-location-parser.js
@@ -0,0 +1,15 @@
+/*
+ * windmill-location-parser
+ * https://github.com/martinnormark/windpowerdk
+ *
+ * Copyright (c) 2014 Martin H. Normark
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+exports.awesome = function() {
+  return 'awesome';
+};
+
+console.log("hello world");
