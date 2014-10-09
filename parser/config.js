97ce7db0f119a8b809e0ae3b66a447823d9c1294
Martin H. Normark <m@martinnormark.com>
2014-10-09 14:00:04 +0200

    Update spreadsheet download URL

--- parser/config.js
+++ parser/config.js
@@ -9,7 +9,7 @@
 'use strict';
 
 module.exports = {
-	dataUrl: 'http://www.ens.dk/sites/ens.dk/files/byggeri/anlaegprodtilnettet.xls',
+	dataUrl: 'http://www.ens.dk/sites/ens.dk/files/info/tal-kort/oversigtstabeller_uk-dk.xls',
 	outputPath: './data/windpowerdk.geojson',
 	utmZone: 32
 };
